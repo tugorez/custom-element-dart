36d72424a14b5cecc5c0993a61c6dede648d6785
Juanjo Tugores <tugorez@gmail.com>
2022-06-12 13:28:46 -0700

    Working with dart2js and improved

--- lib/custom_element.dart
+++ lib/custom_element.dart
@@ -1,22 +1,45 @@
 import 'package:js/js.dart';
 import 'dart:html';
 
-@JS()
-abstract class CustomElement {
+@JS('CustomElement')
+abstract class _CustomElement {
   external void connectedCallback();
 }
 
-@JS()
-external CustomElement createCustomElement(
+@JS('createCustomElement')
+external _CustomElement _createCustomElement(
   OnCreated onCreated,
   OnConnected onConnected,
 );
 
-@JS()
-external void defineCustomElement(
+@JS('defineCustomElement')
+external void _defineCustomElement(
   String tag,
-  CustomElement element,
+  _CustomElement element,
 );
 
 typedef OnCreated = void Function(HtmlElement element);
 typedef OnConnected = void Function();
+
+class CustomElement {
+  final HtmlElement element;
+
+  CustomElement(this.element);
+
+  onConnected() {}
+}
+
+typedef CustomElementCreator = CustomElement Function(HtmlElement);
+
+void defineCustomElement(String tag, CustomElementCreator creator) {
+  late final CustomElement customElement;
+  final foo = _createCustomElement(
+    allowInterop((element) {
+      customElement = creator(element);
+    }),
+    allowInterop(() {
+      customElement.onConnected();
+    }),
+  );
+  _defineCustomElement(tag, foo);
+}
--- web/main.dart
+++ web/main.dart
@@ -1,27 +1,15 @@
 import 'package:custom_element_dart/custom_element.dart';
-import 'package:js/js.dart';
 import 'dart:html';
 
-class HelloWorldElement {
-  final HtmlElement element;
+class HelloWorldElement extends CustomElement {
+  HelloWorldElement(HtmlElement element) : super(element);
 
-  HelloWorldElement(this.element);
-
+  @override
   onConnected() {
     element.innerText = 'Hello World!';
   }
 }
 
 void main() {
-  late final HelloWorldElement element;
-  final customElement = createCustomElement(
-    allowInterop((e) {
-      element = HelloWorldElement(e);
-    }),
-    allowInterop(() {
-      element.onConnected();
-    }),
-  );
-
-  defineCustomElement('hello-world', customElement);
+  defineCustomElement('hello-world', (e) => HelloWorldElement(e));
 }
