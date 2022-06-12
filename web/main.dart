df1f4a5138d6bb50e36f4d92acf10e545c4d0301
Juanjo Tugores <tugorez@gmail.com>
2022-06-11 17:13:10 -0700

    Doesn't work due to dart.wrapType

--- web/main.dart
+++ web/main.dart
@@ -1,5 +1,16 @@
 import 'dart:html';
 
+class HelloWorldElement extends HtmlElement {
+  HelloWorldElement.created() : super.created() {
+    //
+  }
+
+  @override
+  void connectedCallback() {
+    print('Whaaaaaaaaaaaaaaaaat');
+  }
+}
+
 void main() {
-  querySelector('#output')?.text = 'Your Dart app is running.';
+  window.customElements?.define('hello-world', HelloWorldElement);
 }
